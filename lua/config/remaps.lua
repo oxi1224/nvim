d0b96e2246e5efc67845e53a12ba9ae99880298f
ox-i <oxi1224@o2.pl>
2024-05-31 19:57:48 +0200

    Add commenting keybind in visual mode

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -4,9 +4,18 @@ function M.init()
   vim.g.mapleader = " "
   vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
   vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-  vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
   vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
   vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float({ scope="line" }) end)
+
+  -- Comment keybinds
+  vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
+  local esc = vim.api.nvim_replace_termcodes(
+    '<ESC>', true, false, true
+  )
+  vim.keymap.set('x', '<C-_>', function()
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    require('Comment.api').toggle.linewise(vim.fn.visualmode())
+  end)
 end
 
 return M
